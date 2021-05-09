b1dcd24f790a8ee0b4e29c10d7d981ab20a25529
Sahil Saini <46193454+sahil137@users.noreply.github.com>
2021-05-09 16:36:09 +0530

    add style to list item (delete button issue)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
+var taskList = document.getElementsByTagName("li");
+
+var item;
+
+for (item = 0; item < taskList.length; item++) {
+  var span = document.createElement("span");
+  var icon = document.createElement("i");
+  span.className = "delete-task-btn";
+  icon.className = "far fa-trash-alt";
+  span.appendChild(icon);
+  taskList[item].appendChild(span);
+}
+
 function addTask() {
-  var taskDesc = document.getElementById("add-task");
-  var li = document.createElement("li");
-  var text = document.createTextNode(taskDesc.value);
-  li.appendChild(text);
+  var taskDesc = document.getElementById("add-task"); // get input data
+  var li = document.createElement("li"); // create li element
+  var p = document.createElement("p"); // create p element
+
+  if (taskDesc.value === "") {
+    window.alert("Please Enter a task");
+    return;
+  }
+
+  var text = document.createTextNode(taskDesc.value); // create a textnode which holds the input value
+  var checkBox = document.createElement("input"); // create the checkbox
+
+  p.appendChild(text);
+  checkBox.setAttribute("type", "checkbox");
+  li.appendChild(checkBox);
+  li.appendChild(p);
 
   var ul = document.querySelector("ul");
   ul.appendChild(li);
-  taskDesc.value = "";
+  taskDesc.value = ""; // clear the input field
 }
 
 var addTaskButton = document.getElementById("add-task-btn");
